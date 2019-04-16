a65074936e914614ab2153069efa486a285aff77
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 12:19:23 -0600

    Add test to create a dog.

--- spec/requests/api/v1/dogs_request_spec.rb
+++ spec/requests/api/v1/dogs_request_spec.rb
@@ -10,4 +10,16 @@ describe "API Homepage" do
 
     expect(response).to be_successful
   end
+
+  it "can create a dog" do
+    breed = "Labrador"
+    image = "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg"
+
+    post "/api/v1/dogs", params: { breed: breed, image: image }
+    dog = Dog.last
+
+    expect(response).to be_successful
+    expect(dog.breed).to eq(breed)
+    expect(dog.image).to eq(image)
+  end
 end
