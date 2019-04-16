596afd5b41b847c9a1e9672a15b6a2db3c19ce6b
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 13:09:06 -0600

    Adds destroy method in the dogs controller with custom json message.

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -6,4 +6,9 @@ class Api::V1::DogsController < ApplicationController
   def create
     render json: Dog.create(breed: params[:breed], image: params[:image])
   end
+
+  def destroy
+    Dog.delete(params[:id])
+    render json: {success: "Dog id number #{params[:id]} has been deleted"}, status: 200
+  end
 end
