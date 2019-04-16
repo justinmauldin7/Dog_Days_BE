8dc9ade46d0213fa268a1ce99d4e7aa0816b2263
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 12:20:34 -0600

    Adds create method to the dogs controller.

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::DogsController < ApplicationController
   def index
     render json: Dog.all
   end
+
+  def create
+    render json: Dog.create(breed: params[:breed], image: params[:image])
+  end
 end
