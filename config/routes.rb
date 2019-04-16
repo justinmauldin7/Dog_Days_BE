791a3e7d1d31143854c11b5ba5fc4df2e2b645bb
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 12:19:48 -0600

    Added the create route for dogs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :dogs, only: [:index]
+      resources :dogs, only: [:index, :create]
     end
   end
 end
