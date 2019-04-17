6de8f0bd9c4c4439226a5e4c497ce82c818a84e1
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 22:58:35 -0600

    Updates the dogs controller to account for users and adds 401 errors for unsuccessful actions.

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,14 +1,29 @@
 class Api::V1::DogsController < ApplicationController
   def index
-    render json: Dog.all
+    user = User.find_by(id: params[:user_id])
+    if user && user.dogs.count > 0
+      render json: user.dogs
+    else
+      render json: {error: "401 Unauthorized"}, status: 401
+    end
   end
 
   def create
-    render json: Dog.create(breed: params[:breed], image: params[:image])
+    user = User.find_by(id: params[:user_id])
+    if user
+      render json: Dog.create(user: user, breed: params[:breed], image: params[:image])
+    else
+      render json: {error: "401 Unauthorized"}, status: 401
+    end
   end
 
   def destroy
-    Dog.delete(params[:id])
-    render json: {success: "Dog id number #{params[:id]} has been deleted"}, status: 200
+    user = User.find_by(id: params[:user_id])
+    if user
+      Dog.delete(params[:id])
+      render json: {success: "Dog id number #{params[:id]} has been deleted from user_id #{user.id}"}, status: 200
+    else
+      render json: {error: "401 Unauthorized"}, status: 401
+    end
   end
 end
