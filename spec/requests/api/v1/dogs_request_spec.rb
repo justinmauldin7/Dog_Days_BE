16cc5eaddb9210a6ece7a2deb971c9bb24bfcebc
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 22:57:50 -0600

    Updates dog request test to account for users being attached to dogs.

--- spec/requests/api/v1/dogs_request_spec.rb
+++ spec/requests/api/v1/dogs_request_spec.rb
@@ -2,37 +2,42 @@ require 'rails_helper'
 
 describe "API Homepage" do
   it "can navigate to the homepage" do
-    Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
-    Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_8051.jpg")
-    Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_7411.jpg")
-    Dog.create(breed: "Beagle", image: "https://images.dog.ceo/breeds/beagle/n02088364_12124.jpg")
-    Dog.create(breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_13439.jpg")
-    Dog.create(breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_14594.jpg")
+    user_1 = User.create(name: "Justin", email: "justin@gmail.com", password: "123")
 
-    get '/api/v1/dogs'
+    Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
+    Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_8051.jpg")
+    Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_7411.jpg")
+    Dog.create(user: user_1, breed: "Beagle", image: "https://images.dog.ceo/breeds/beagle/n02088364_12124.jpg")
+    Dog.create(user: user_1, breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_13439.jpg")
+    Dog.create(user: user_1, breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_14594.jpg")
+
+    get '/api/v1/dogs' , params: { user_id: user_1.id }
 
     expect(response).to be_successful
     expect(Dog.count).to eq(6)
   end
 
   it "can create a dog" do
+    user_1 = User.create(name: "Justin", email: "justin@gmail.com", password: "123")
     breed = "Labrador"
     image = "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg"
 
-    post "/api/v1/dogs", params: { breed: breed, image: image }
+    post "/api/v1/dogs", params: { user_id: user_1.id, breed: breed, image: image }
     dog = Dog.last
 
     expect(response).to be_successful
+    expect(dog.user).to eq(user_1)
     expect(dog.breed).to eq(breed)
     expect(dog.image).to eq(image)
   end
 
   it "can delete a dog" do
-    dog = Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
+    user_1 = User.create(name: "Justin", email: "justin@gmail.com", password: "123")
+    dog = Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
 
     expect(Dog.count).to eq(1)
 
-    delete "/api/v1/dogs/#{dog.id}"
+    delete "/api/v1/dogs/#{dog.id}", params: { user_id: user_1.id }
 
     expect(response).to be_successful
     expect(Dog.count).to eq(0)
