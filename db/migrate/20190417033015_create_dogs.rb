8cd87cd3cf8c641996b6fb63da6bf3cd3e98aea3
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 21:46:10 -0600

    Adds migration for users & updates the dogs migration.

--- db/migrate/20190417033015_create_dogs.rb
+++ db/migrate/20190417033015_create_dogs.rb
@@ -1,6 +1,7 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
+      t.references :user, foreign_key: true
       t.string :breed
       t.string :image
 
