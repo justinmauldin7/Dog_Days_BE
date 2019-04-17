a7d422dea526e2ade858584660221a0095e6620e
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 21:46:47 -0600

    Updates seed file to now have users & dogs attached to a user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Dog.destroy_all
 
-Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
-Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_8051.jpg")
-Dog.create(breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_7411.jpg")
-Dog.create(breed: "Beagle", image: "https://images.dog.ceo/breeds/beagle/n02088364_12124.jpg")
-Dog.create(breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_13439.jpg")
-Dog.create(breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_14594.jpg")
+user_1 = User.create!(name: "Justin", email: "justin@gmail.com", password: "123")
+
+Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg")
+Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_8051.jpg")
+Dog.create(user: user_1, breed: "Labrador", image: "https://images.dog.ceo/breeds/labrador/n02099712_7411.jpg")
+Dog.create(user: user_1, breed: "Beagle", image: "https://images.dog.ceo/breeds/beagle/n02088364_12124.jpg")
+Dog.create(user: user_1, breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_13439.jpg")
+Dog.create(user: user_1, breed: "Pug", image: "https://images.dog.ceo/breeds/pug/n02110958_14594.jpg")
+
+p "Created #{User.count} users"
+p "Created #{Dog.count} dogs"
