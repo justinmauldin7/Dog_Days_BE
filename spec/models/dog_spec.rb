0ac61d7972ef1a2ce04a66f4fb3f014b0724350c
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 21:47:31 -0600

    Adds relationship to dog model tests.

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  it { should validate_presence_of(:breed) }
-  it { should validate_presence_of(:image) }
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+  describe 'validations' do
+    it { should validate_presence_of(:breed) }
+    it { should validate_presence_of(:image) }
+  end
 end
